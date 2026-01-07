4c733afebe4609a1bd0912a1d7f2ae2fc5946f2b
Thomas Christiansen <thomasc1993@gmail.com>
2026-01-07 11:49:49 +0100

    Update telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,23 +11,26 @@ return {
         }
         require('telescope').load_extension('fzf')
 
-        vim.keymap.set('n', '<leader>tf',
+        vim.keymap.set('n', '<leader>ff',
             function() require('telescope.builtin').find_files() end,
             { noremap = true, desc = "Search files" })
-        vim.keymap.set('n', '<leader>tg',
+        vim.keymap.set('n', '<leader>fb',
+            function() require('telescope.builtin').buffers { sort_mru = true } end,
+            { noremap = true, desc = "Search buffers" })
+        vim.keymap.set('n', '<leader>fg',
             function() require('telescope.builtin').live_grep() end,
             { noremap = true, desc = "Grep files" })
-        vim.keymap.set('n', '<leader>th',
+        vim.keymap.set('n', '<leader>fh',
             function() require('telescope.builtin').help_tags() end, { noremap = true, desc = "Search help tags" })
-        vim.keymap.set('n', '<leader>tn', function()
+        vim.keymap.set('n', '<leader>fn', function()
             require('telescope.builtin').find_files {
                 cwd = vim.fn.stdpath('config')
             }
         end, { noremap = true, desc = "Search nvim config" })
-        vim.keymap.set('n', '<leader>tr', function()
+        vim.keymap.set('n', '<leader>fr', function()
             require('telescope.builtin').lsp_references()
         end, { noremap = true, desc = "Search LSP references under cursor" })
-        vim.keymap.set('n', '<leader>ts', function()
+        vim.keymap.set('n', '<leader>fs', function()
             require('telescope.builtin').lsp_document_symbols()
         end, { noremap = true, desc = "Search LSP document symbols" })
     end
