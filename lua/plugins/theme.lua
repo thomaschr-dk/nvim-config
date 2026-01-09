64f82cf05f4e33c9efe7dbf7746c8f974f79d43f
Thomas Christiansen <thomasc1993@gmail.com>
2026-01-09 16:30:20 +0100

    Dim inactive window

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,6 +3,9 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+        require('gruvbox').setup {
+            dim_inactive = true
+        }
         vim.cmd [[colorscheme gruvbox]]
     end
 }
